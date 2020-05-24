372760003ce42322e46b4d2a11e0f43938a1850b
zHaytam <haytam.zanid@gmail.com>
2020-05-24 17:58:17 +0100

    Add AddBlazux extension method

---
+++ src/Blazux.Web/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Blazux.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Blazux.Web
+{
+    public static class Extensions
+    {
+
+        public static IServiceCollection AddBlazux<TState>(this IServiceCollection services,
+            Action<StoreBuilder<TState>> configurator, TState initialState = default)
+        {
+            var builder = new StoreBuilder<TState>(initialState);
+            configurator.Invoke(builder);
+
+            services.AddSingleton<IStore<TState>>(builder.Store);
+
+            return services;
+        }
+
+    }
+}
---
+++ src/Blazux.Web/StoreBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Blazux.Core;
+using System;
+using System.Reflection;
+
+namespace Blazux.Web
+{
+    public class StoreBuilder<TState>
+    {
+
+        internal Store<TState> Store { get; }
+
+        internal StoreBuilder(TState initialState = default)
+        {
+            Store = new Store<TState>(initialState);
+        }
+
+        public void AddReducer<TAction>(Func<TState, TAction, TState> reducer) where TAction : IAction
+            => Store.AddReducer(reducer);
+
+        public void AddReducersFromAssembly(Assembly assembly, params Type[] typesToExclude)
+            => Store.AddReducersFromAssembly(assembly, typesToExclude);
+
+    }
+}
